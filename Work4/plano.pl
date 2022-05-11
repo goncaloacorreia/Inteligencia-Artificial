9f1d96025cca09de8d5186dd0efbf606ac3c6d48
goncaloacorreia <gac632000@gmail.com>
2022-05-11 13:01:46 +0100

    work4

--- Work4/plano.pl
+++ Work4/plano.pl
@@ -8,20 +8,21 @@ estado_inicial([
         ]).
 
 %alterar consoante o estado final pretendido
-estado_final(F):- estado_1(F).
+estado_final(F):- estado1(F).
 
-estado_1([
+estado1([
             val(a, vb),
             val(b, va),
-            val(c, vb),
-            val(d, vd),
-            val(e, ve)
+            val(c, vb)
         ]).
 
-estado_2([
+estado2_1([ 
             val(a, vb),
+            val(c, mais(va, vb))
+        ]).
+
+estado2_2([ 
             val(b, va),
-            soma(c, va, vb),
             val(d, vc),
             val(e, va)
         ]).
@@ -31,10 +32,10 @@ accao(afetar_r(R1, R2), [val(R1, X), val(R2, Y)], [val(R1, Y)], [val(R1, X)] ) :
                                                                             member(X, [va, vb, vc, vd, ve]),
                                                                             member(Y, [va, vb, vc, vd, ve]).
                                                                             
-%alterar ! ! !
-accao(somar_r(R1, R2, R3), [val(R1, X), val(R2, Y), val(R3, Z)], [soma(R1, Y, Z)], [val(R1, X)]) :- member(R1, [a, b, c, d, e]),
+
+accao(somar_r(R1, R2, R3), [val(R1, X1), val(R2, Y), val(R3, Z)], [val(R1, X)], [val(R1, X1)]) :- member(R1, [a, b, c, d, e]),
                                                                                                     member(R2, [a, b, c, d, e]),
                                                                                                     member(R3, [a, b, c, d, e]),
-                                                                                                    member(X, [va, vb, vc, vd, ve]),
                                                                                                     member(Y, [va, vb, vc, vd, ve]),
-                                                                                                    member(Z, [va, vb, vc, vd, ve]).
+                                                                                                    member(Z, [va, vb, vc, vd, ve]),
+                                                                                                    X = mais(Y,Z).
--- Work4/pop.pl
+++ Work4/pop.pl
@@ -3,125 +3,129 @@
 n(3).
 
 
-plano_ini(p([s1-op(inicial, [], EstadoIni, []), s2-op(final, EstadoFin, [], [])], [m(s1, s2)], [])):- estado_inicial(EstadoIni),
-                                                                                                      estado_final(EstadoFin).
+% POP - Planeador de ordem parcial
+
+% representacao de um plano:
+% plano(p(Passos,Ordem,Links))
+
+% representacao de um passo:
+% passo(nomePasso-op(nomeAccao,Precond,AddList,DeleList)).
 
 
-plano(P):- plano_ini(Pi), pop(Pi, P2), lineariza(P2, P).
+% plano_ini(P) constroi o plano inicial dados os estados inicial e final
 
 
-pop(Pi, P):- escolheCi(Pi, Si, Ci, Pj),
-             !,
-             escolheSk(Pj, Si, Ci, Pk),
-             resolveAmeacas(Pk, Pl),
-             consistentep(Pl),
-             pop(Pl, P).
+plano_ini(p([s1-op(inicial,[],EstadoIni,[]),
+s2-op(final,EstadoFin,[],[])],[m(s1,s2)],[]))
+:-estado_inicial(EstadoIni),
+estado_final(EstadoFin).
+
+
+% este predicado retorna um plano linearizado
+
+plano(P):- plano_ini(Pi), pop(Pi,P2), write(plano(P2)), nl, lineariza(P2,P).
+
+% pop(+Pi,-P)
+% este predicado recebe um plano e retorna um novo plano com mais uma
+% condicao satisfeita
+
+pop(Pi,P):- escolheCi(Pi,Si,Ci,Pj),!,
+escolheSk(Pj,Si,Ci,Pk),
+resolveAmeacas(Pk,Pl),
+consistentep(Pl),
+pop(Pl,P).
 
 pop(P,P).
 
+%Escolhe um condicao, Ci, da lista de precondicoes de um passo, Si, do plano
+%que ainda nao esta satisfeita
 
-escolheCi(p(Passos, Ordem, Links), Si, Ci, p([Si-op(Nome, Cond1, Add, Del)|Passos1], Ordem, Links)):- retira(Si-op(Nome, Cond, Add, Del), Passos, Passos1),
-                                                                                                      retira(Ci, Cond, Cond1).
-                                                                                                      %write(escolheu(Si, Ci)),
-                                                                                                      %nl.
-
-
-escolheSk(p(Passos, Ordem, Links), S, C, p(Passos, [m(Sk, S)|Ordem], [link(Sk, S, C)|Links])):- member(Sk-op(_, _, Add, _), Passos),
-                                                                                                member(C, Add).
-                                                                                                %write(links([link(Sk, S, C)|Links])),
-                                                                                                %nl.
+escolheCi(p(Passos,Ordem,Links),Si,Ci,
+p([Si-op(Nome,Cond1,Add,Del)|Passos1], Ordem,Links)):-
+retira(Si-op(Nome,Cond,Add,Del),Passos,Passos1),
+retira(Ci,Cond,Cond1).
 
 
-escolheSk(p(Passos, Ordem, Links), S, C, p([Sk-op(N, Cond, Add, Del)|Passos], [m(Sk, S), m(Sk, s2), m(s1, Sk)|Ordem], [link(Sk, S, C)|Links])):- novo(Sk),
-                                                                                                                                                 !,
-                                                                                                                                                 length(Passos, M),
-                                                                                                                                                 M < 12,
-                                                                                                                                                 !,
-                                                                                                                                                 accao(N, Cond, Add, Del),
-                                                                                                                                                 member(C, Add).
+%escolhe uma passo do plano, Sk, que satisfaz Ci, acrescenta um link e
+%e a ordem relativa entre os passos do plano Sk<Si.
+
+escolheSk(p(Passos,Ordem,Links),S,C,p(Passos,[m(Sk,S)|Ordem],
+[link(Sk,S,C)|Links])):-
+member(Sk-op(_,_,Add,_),Passos),
+member(C, Add).
+
+%Cria um novo passo no plano de forma a satisfazer Ci, actualiza os links
+%e a Ordem entre os passos.
+
+escolheSk(p(Passos,Ordem,Links),S,C,p([Sk-op(N,Cond,Add,Del)|Passos],
+[m(Sk,S),m(Sk,s2),m(s1,Sk)|Ordem],
+[link(Sk,S,C)|Links])):-
+novo(Sk),!,length(Passos,M),M<5,!, %alterar este valor consoante o número de passos
+accao(N,Cond,Add,Del),
+member(C, Add).
 
 
-resolveAmeacas(p(Passos, Ordem, Links), P):- member(link(S1, S2, C), Links),
-                                             member(S3-op(_, _, _, Del), Passos),
-                                             \+ member(S3, [S1, S2]),
-                                             member(C, Del),
-                                             ameaca(S1, S2, S3, Ordem),
-                                             !,
-                                             resolve(S1, S2, S3, Rest),
-                                             write(ameaca(S1, S2, S3, C)), nl,
-                                             resolveAmeacas(p(Passos, [Rest|Ordem], Links), P).
+%resolve as ameacas aos links
 
-resolveAmeacas(P, P).
+resolveAmeacas(p(Passos,Ordem,Links),P):-
+member(link(S1,S2,C),Links),
+member(S3-op(_,_,_,Del),Passos),\+ member(S3,[S1,S2]),
+member(C,Del),ameaca(S1,S2,S3,Ordem),!,
+resolve(S1,S2,S3,Rest),
+resolveAmeacas(p(Passos,[Rest|Ordem],Links),P).
 
+resolveAmeacas(P,P).
 
-ameaca(S1, S2, S3, Ord):- consistente([m(S1, S3), m(S3, S2)|Ord]).
-
-
-resolve(S1, _, S3, m(S3, S1)).
-
-resolve(_, S2, S3, m(S2, S3)).
+%S3 ameaca o link(S1,S2,C) se S3 tem C na delete list e S1<S3<S2
+ameaca(S1,S2,S3,Ord):- consistente([m(S1,S3),m(S3,S2)|Ord]).
 
 
-retira(X, [X|R], R).
-
-retira(X, [Y|R], [Y|S]):- retira(X, R, S).
-
-
-novo(O):- retract(n(I)),
-          J is I + 1,
-          asserta(n(J)),
-          name(I, L),
-          name(O, [115|L]),
-          !.
+%resolve ameaca: promove ou despromove S3
+resolve(S1,_,S3,m(S3,S1)).
+resolve(_,S2,S3,m(S2,S3)).
 
 
-consistentep(p(_, Ordem, _)):- consistente(Ordem).
+retira(X,[X|R],R).
+retira(X,[Y|R],[Y|S]):- retira(X,R,S).
+
+%retorna um novo identificador para um passo do plano
+novo(O):- retract(n(I)), J is I+1, asserta(n(J)),
+name(I,L), name(O,[115|L]),!.
+
+%---Os predicados abaixo permitem verificar se um plano e consistente
+% e linearizar uma plano
+
+%verifica se um Plano e consistente
+consistentep(p(_,Ordem,_)):- consistente(Ordem).
+
+%verifica se uma ordem e consistente
+consistente(A):-renomeia(A,[],B,_), ve_consistente(B).
 
 
-consistente(A):-renomeia(A, [], B, _), ve_consistente(B).
-
-
-renomeia([], N, [], N).
-
-renomeia([m(A, B)|R], Nomes, [m(X, Y)|S], Nomesf):- nome(A, X, Nomes, Nomes1),
-                                                    nome(B, Y, Nomes1, Nomes2),
-                                                    renomeia(R, Nomes2, S, Nomesf).
-
-
-nome(A, X, L, L):- member(n(X, A), L),
-                   !.
-
-nome(A, X, L, [n(X, A)|L]).
-
+renomeia([],N,[],N).
+renomeia([m(A,B)|R],Nomes,[m(X,Y)|S],Nomesf):- 
+nome(A,X,Nomes,Nomes1),
+nome(B,Y,Nomes1,Nomes2),
+renomeia(R,Nomes2,S,Nomesf).
+nome(A,X,L,L):- member(n(X,A),L),!.
+nome(A,X,L,[n(X,A)|L]).
 
 ve_consistente([]).
+ve_consistente([m(A,B)|R]):- A#>=0, A #=< 30, B#>=0, B #=<30, A #< B,
+ve_consistente(R).
 
-ve_consistente([m(A, B)|R]):- A #>= 0,
-                              A #=< 30,
-                              B #>=0,
-                              B #=<30,
-                              A #< B,
-                              ve_consistente(R).
+lineariza(p(Passos,Ordem,_Links),P):-
+renomeia(Ordem,[],OrdemVar,Nomes),
+length(Nomes,N), member(n(X,s2),Nomes), X#>=0, X #=< N,
+ve_consistente(OrdemVar),variaveis(Nomes,Vars),
+fd_labelingff(Vars),
+sort(Nomes,Ord),
+plano_ord(Ord,Passos,P).
 
-
-lineariza(p(Passos, Ordem, _Links), P):- renomeia(Ordem, [], OrdemVar, Nomes),
-                                         length(Nomes, N),
-                                         member(n(X, s2), Nomes),
-                                         X #>= 0,
-                                         X #=< N,
-                                         ve_consistente(OrdemVar),
-                                         variaveis(Nomes, Vars),
-                                         fd_labelingff(Vars),
-                                         sort(Nomes, Ord),
-                                         plano_ord(Ord, Passos,P).
+variaveis([],[]).
+variaveis([n(X,_)|R],[X|S]):- variaveis(R,S).
 
 
-variaveis([], []).
-
-variaveis([n(X, _)|R], [X|S]):- variaveis(R, S).
-
-
-plano_ord([], _, []).
-
-plano_ord([n(_, S)|R], Passos, [S-N|Passos2]):- member(S-op(N, _, _, _), Passos),
-                                                plano_ord(R, Passos, Passos2).
+plano_ord([],_,[]).
+plano_ord([n(_,S)|R],Passos,[S-N|Passos2]):- member(S-op(N,_,_,_),Passos),
+plano_ord(R,Passos,Passos2).
